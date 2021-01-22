b3dd2a2e054a74eb621ecde10ecf3ff3cf900c72
alessandro <alessandro.massarella@gmail.com>
2021-01-22 17:36:25 +0100

    Routes + Blade - base
    una prima versione molto basilare

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,16 @@
 <!-- SEZIONE ABOUT -->
+@extends('layouts.app')    <!-- RICHIAMO IL LAYOUT CREATO -->
+
+@section('main')   <!-- IN QUESTA SEZIONE QUESTA VOLTA CI SARÀ LA SEZIONE ABOUT ME -->
+
 <section>
 
     <a href="/">HOME</a>
 
+    <h3>ABOUT ME</h3>
+
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore est optio, ipsum voluptate aliquid repudiandae illum laborum ab vitae sint. Iste exercitationem voluptate recusandae? Eveniet praesentium fugiat omnis dolorum quasi.</p>
 
-</section>
+</section>  
+
+@endsection     <!-- CHIUSURA DELLA SEZIONE -->
